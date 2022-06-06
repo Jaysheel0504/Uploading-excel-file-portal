bbf0cbd9e33b542a781ceee0283e859ec7ed2beb
Jaysheel <jaysheelshah2002@gmail.com>
2022-06-07 01:47:35 +0530

    commit6

--- sheetupload/projectapp/views.py
+++ sheetupload/projectapp/views.py
@@ -5,26 +5,30 @@ from tablib import Dataset
 from .models import Person
 
 
-
+def export(request):
+    person_resource = PersonResource()
+    dataset = person_resource.export()
+    response = HttpResponse(dataset.csv, content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="merged.csv"'
+    return response
 
 
 def simple_upload(request):
     if request.method == 'POST':
         person_resource = PersonResource()
         dataset = Dataset()
-        new_persons = request.FILES['myfile']
+        new_persons = request.FILES.getlist('myfile')
+        for person in new_persons: 
+            imported_data = dataset.load(person.read(), format='xlsx')
 
-        imported_data = dataset.load(new_persons.read(), format='xlsx')
-
-        for data in imported_data:
-            print(data[1])
-            value = Person(
-                data[0],
-                data[1],
-                data[2],
-                data[3]
-            )
-            value.save()
+            for data in imported_data:
+                value = Person(
+                    data[0],
+                    data[1],
+                    data[2],
+                    data[3]
+                )
+                value.save()
 
 
 
